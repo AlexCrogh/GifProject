e2673c9a5c75bcee10a1feeb777c92ef545ee043
Alex Croghan <31134800+AlexCrogh@users.noreply.github.com>
2021-06-21 12:43:47 -0400

    Twitter wrapper added. Cron times updated. More subreddits added.

--- src/main/java/com/croghan/gifs/Gif.java
+++ src/main/java/com/croghan/gifs/Gif.java
@@ -6,8 +6,10 @@ import java.io.Serializable;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+
 @Entity
 public class Gif {
+
 
     @Id//@GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
--- src/main/java/com/croghan/gifs/ScheduledTasks.java
+++ src/main/java/com/croghan/gifs/ScheduledTasks.java
@@ -1,11 +1,5 @@
 package com.croghan.gifs;
 
-import oauth.signpost.OAuthConsumer;
-import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONObject;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,21 +7,22 @@ import org.springframework.web.client.RestTemplate;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
-import org.apache.commons.io.IOUtils;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 @RestController
 public class ScheduledTasks {
 
-    private static String consumerKeyStr = "eoDuWMwfMSczkEhnIJYwwxE0q";
-    private static String consumerSecretStr = "GtelEhKwoMqnBZAI0vcsoQdpuixLzQ2zkHEwmkDoGovLfca97z";
-    private static String accessTokenStr = "1052297317267755008-5NZ8mfN31SVJOFH2gt8GlHrbhZyobE";
-    private static String accessTokenSecretStr = "mCWe1KiULLblX6e8BqB7BmrXvd7PL8H8MuNUoTakpanL2";
+    private static String consumerKeyStr = "LQuDXRsc2NYeobJoyHQMdQlo1";
+    private static String consumerSecretStr = "po6aOSwnE3o03wgjHABT5OMgbe5Ah3FRkwaBd2Qqgyny6Tq8aW";
+    private static String accessTokenStr = "1052297317267755008-grBQs7l1WqUdRUfvuinn1svUm0rZNp";
+    private static String accessTokenSecretStr = "pTThV5mpLmif7I2EIV1lqrTehto1CiaW7ZqmMeYWatWQ9";
 
     int id = 1;
     static RestTemplate restTemplate = new RestTemplate();
 
-    @Scheduled(cron = "0 0 0 * * * ")
+    @Scheduled(cron = "@weekly ")
     public void addGifOne() throws IOException {
         System.out.println("attempting to retrieve gif from /r/gifs");
         Gif gif = getGif(id, "gifs");
@@ -36,39 +31,53 @@ public class ScheduledTasks {
         System.out.println("id now " + id);
     }
 
-    @Scheduled(cron = "0 0 0 * * * ")
+    @Scheduled(cron = "@weekly")
     public void addGifTwo() throws IOException {
         Gif gif = getGif(id, "gifrecipes");
         String url = "http://localhost:8080/createGif";
         restTemplate.postForObject(url, gif, Gif.class);
         System.out.println("Post saved to db " + gif.getTitle());
     }
-    @Scheduled(cron = "0 0 0 * * * ")
+    @Scheduled(cron = "@weekly")
     public void addGifThree() throws IOException {
         Gif gif = getGif(id, "educationalgifs");
         String url = "http://localhost:8080/createGif";
         restTemplate.postForObject(url, gif, Gif.class);
         System.out.println("Post saved to db " + gif.getTitle());
     }
+    @Scheduled(cron = "@weekly")
+    public void addGifFour() throws IOException {
+        Gif gif = getGif(id, "bettereveryloop");
+        String url = "http://localhost:8080/createGif";
+        restTemplate.postForObject(url, gif, Gif.class);
+        System.out.println("Post saved to db " + gif.getTitle());
+    }
+    @Scheduled(cron = "@weekly")
+    public void addGifFive() throws IOException {
+        Gif gif = getGif(id, "perfectloops");
+        String url = "http://localhost:8080/createGif";
+        restTemplate.postForObject(url, gif, Gif.class);
+        System.out.println("Post saved to db " + gif.getTitle());
+    }
 
 
-    @Scheduled(cron = "0 0 */12 * * * ")
+    @Scheduled(cron = "0 0 0 * * MON,WED,FRI")
     public void postToTwitter() throws Exception {
-        OAuthConsumer oAuthConsumer = new CommonsHttpOAuthConsumer(consumerKeyStr, consumerSecretStr);
-        oAuthConsumer.setTokenWithSecret(accessTokenStr, accessTokenSecretStr);
         Gif gif = restTemplate.getForObject("http://localhost:8080/getTwitterGif", Gif.class);
         System.out.println("Post retrieved from DB: " + gif.getTitle() + "-- id: " + gif.getId());
         if(gif.getTitle() != null){
-            String str = URLEncoder.encode(gif.getTitle() +" (via reddit.com/r/" +  gif.getCategory()+")\n" + gif.getUrl(), "UTF-8");
-            HttpPost httpPost = new HttpPost("http://api.twitter.com/1.1/statuses/update.json?status=" + str );
-            oAuthConsumer.sign(httpPost);
+            String str = gif.getTitle() +" (via reddit.com/r/" +  gif.getCategory()+")\n" + gif.getUrl();
+            ConfigurationBuilder cb = new ConfigurationBuilder();
+            cb.setDebugEnabled(true)
+                    .setOAuthConsumerKey(consumerKeyStr)
+                    .setOAuthConsumerSecret(consumerSecretStr)
+                    .setOAuthAccessToken(accessTokenStr)
+                    .setOAuthAccessTokenSecret(accessTokenSecretStr);
+            TwitterFactory tf = new TwitterFactory(cb.build());
+            Twitter twitter = tf.getInstance();
+            twitter.updateStatus(str);
             gif.setPosted(true);
             restTemplate.put("http://localhost:8080/updateGif", gif);
-            HttpClient httpClient = new DefaultHttpClient();
-            HttpResponse httpResponse = httpClient.execute(httpPost);
-            int statusCode = httpResponse.getStatusLine().getStatusCode();
-            System.out.println(statusCode + ':' + httpResponse.getStatusLine().getReasonPhrase());
-            System.out.println(IOUtils.toString(httpResponse.getEntity().getContent()));
         }
         else{
             System.out.println("Post object was null");
@@ -78,7 +87,8 @@ public class ScheduledTasks {
 
     public Gif getGif(int id, String sub) throws IOException {
         //call pushshift api
-        URL url = new URL("http://api.pushshift.io/reddit/search/submission/?subreddit="+ sub + "&after=1d&sort=desc&sort_type=num_comments&is_video=false&size=1");
+        URL url = new URL("https://api.pushshift.io/reddit/search/submission/?subreddit="+ sub +
+                "&after=1w&sort=desc&sort_type=num_comments&is_video=false&over_18=false&stickied=false&size=1");
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
         InputStream inputStream = new BufferedInputStream(urlConnection.getInputStream());
         BufferedReader inputReader = new BufferedReader(new InputStreamReader(inputStream));
